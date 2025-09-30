cc7d86491ac1be78ad738ec44455f55daeddaf95
rainbown1 <milcacelestenavadedios@gmail.com>
2025-09-29 18:00:02 -0600

    Clase productos

--- Inventario/src/App.java
+++ Inventario/src/App.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
+import Clases.Producto;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner s = new Scanner(System.in);
+        Producto p = new Producto();
+        Producto[] productos = new Producto[5];
+
+        productos[0] = new Producto("Laptop", 12500.50, 5);
+        productos[1] = new Producto("Mouse", 250.75, 20);
+        productos[2] = new Producto("Teclado", 450.00, 15);
+        productos[3] = new Producto("audifonos", 300.0, 30);
+        productos[4] = new Producto("Cargador", 3000.0, 28);
+
+        
+        p.getInventario(productos);
+
+         for (Producto producto : productos) {
+            System.out.println(producto);
+        }
+
+        System.err.println("Buscar un proucto: ");
+        String nombre = s.nextLine();
+        p.buscarProducto(productos, nombre);
     }
 }
--- Inventario/src/Clases/Producto.java
+++ Inventario/src/Clases/Producto.java
@@ -2,4 +2,67 @@ package Clases;
 
 public class Producto {
 
+    private String nombre;
+    private double precio;
+    private int stock;
+
+    public Producto(String nombre, double precio, int stock){
+        this.nombre = nombre;
+        this.precio = precio;
+        this.stock = stock;
+    }
+    public Producto(){}
+
+    public void getInventario(Producto[] products){
+        for (int i = 1; i < products.length - 1; i++) {
+            for (int j = 0; j < products.length - i; j++) {
+                if (products[j].getPrecio() > products[j + 1].getPrecio()) {
+                    Producto max = products[j];
+                    products[j] = products[j+1];
+                    products[j+1] = max;
+                }
+            }
+        }
+    }
+
+    public boolean buscarProducto(Producto[] producto, String nombre){
+        boolean encontrado = false;
+        for (int i = 0; i < producto.length; i++) {
+           if (producto[i].getName().equalsIgnoreCase(nombre)) {
+                System.out.println("Producto encontrado");
+                encontrado = true;
+                System.out.println(producto[i]);
+                break;
+           }
+           
+        }
+        if (!encontrado) {
+            System.out.println("Producto no encontrado");
+           }
+        return encontrado;
+    }
+    
+    public String getName(){
+        return nombre;
+    }
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    public double getPrecio(){
+        return precio;
+    }
+    public void setPrecio(double precio){
+        this.precio = precio;
+    }
+    public int getStock(){
+        return stock;
+    }
+    public void setStock(int stock){
+        this.stock = stock;
+    }
+    @Override
+    public String toString() {
+        return "Nombre: " + nombre + "  Precio: $" + precio + "  Stock: " + stock;
+    }
+
 }
