32c595a5e40a1f009e650ec3e67f853a35f17663
rainbown1 <milcacelestenavadedios@gmail.com>
2025-10-14 12:33:25 -0600

    Clase lista

--- Inventario/src/Lista.java
+++ Inventario/src/Lista.java
@@ -1,3 +1,20 @@
 public class Lista {
 
+    private Nodo cabeza;
+
+    public Lista(Nodo cabeza){
+        this.cabeza = cabeza;
+    }
+
+    public boolean vacia(){
+        if(cabeza == null){
+            return true;
+        }
+        return false;
+    }
+    public void agregar(){
+        Nodo nuevoNodo = new Nodo();
+        nuevoNodo.setSiguiente(cabeza);
+        cabeza = nuevoNodo;
+    }
 }
--- Inventario/src/Nodo.java
+++ Inventario/src/Nodo.java
@@ -1,3 +1,9 @@
 public class Nodo {
 
+    public Nodo(){
+
+    }
+    public void setSiguiente(Nodo nodo){
+
+    }
 }
